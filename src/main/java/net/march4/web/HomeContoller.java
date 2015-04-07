caf8ef62084cbf992f373870f7f4a1f510afa1b4
Deplax <Deplax@JeongWonui-MacBook-Pro.local>
2015-04-07 14:49:57 +0900

    pom.xml 정리 logger 추가 및 정

--- src/main/java/net/march4/web/HomeContoller.java
+++ src/main/java/net/march4/web/HomeContoller.java
@@ -1,13 +1,17 @@
 package net.march4.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeContoller {
+	private static final Logger log = LoggerFactory.getLogger(HomeContoller.class);
 
 	@RequestMapping("/")
 	public String home(){
+		log.debug("로그 실행!!");
 		return "index";
 	}
 	
