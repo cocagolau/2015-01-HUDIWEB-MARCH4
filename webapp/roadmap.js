159201dc5c380672d8c9297b75e2917525bbe97b
heesu <heesu@nhnnext.org>
2015-04-07 15:04:43 +0900

    draggable

--- webapp/roadmap.js
+++ webapp/roadmap.js
@@ -1,4 +1,33 @@
 jQuery(document).ready(function($) {
+    $('.sortableContainer').on('mousedown', '.sortable', function(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        var initEl = $(this);
+        var initElPos = getPosition(this);
+        var initClickPos = {pageX:e.pageX, pageY:e.pageY};
+        console.log(initClickPos.pageX, initClickPos.pageY);
+
+        $(document).on('mousemove', function(e) {
+            var deltaClickPos = {pageX:e.pageX-initClickPos.pageX,
+                pageY:e.pageY-initClickPos.pageY};
+            initEl.css({"position": "relative",
+            "top" : initElPos.top + deltaClickPos.pageY,
+            "left": initElPos.left + deltaClickPos.pageX});
+        });
+    });
+
+    $(document).on('mouseup', function(e) {
+        $(document).off('mousemove');
+    });
+
+    var getPosition = function(el) {
+        var position = {
+            top : parseInt($(el).css('top')) || 0,
+            left : parseInt($(el).css('left')) || 0
+        };
+        return position;
+    };
 });
 
 angular.module('roadmap', [])
