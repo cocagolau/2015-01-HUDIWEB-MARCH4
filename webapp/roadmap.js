ceb53b6d081d14b7d0d28a729cff15092722cebc
heesu <heesu@nhnnext.org>
2015-04-07 17:02:11 +0900

    get mouseovered

--- webapp/roadmap.js
+++ webapp/roadmap.js
@@ -6,16 +6,46 @@ jQuery(document).ready(function($) {
         var initEl = $(this);
         var initElPos = getPosition(this);
         var initClickPos = {pageX:e.pageX, pageY:e.pageY};
-        console.log(initClickPos.pageX, initClickPos.pageY);
 
         $(document).on('mousemove', function(e) {
+            // init (//for check)
+            $('.sortable').css("backgroundColor", 'white');
+
             var deltaClickPos = {pageX:e.pageX-initClickPos.pageX,
                 pageY:e.pageY-initClickPos.pageY};
+
+
+            var result = getMouseoveredIndex(e, $('.sortable'), initEl[0]);
+            if(result >= 0) {
+                // for check
+                $('.sortable').eq(result).css("backgroundColor", 'red');
+            }
+
+
             initEl.css({"position": "relative",
             "top" : initElPos.top + deltaClickPos.pageY,
             "left": initElPos.left + deltaClickPos.pageX});
         });
     });
+
+    var getMouseoveredIndex = function(e, jList, self) {
+        // 일단 세로만.
+        for(var i=0; i<jList.length; i++) {
+            console.log(self);
+            if(jList[i] === self) continue;
+            var elY1, elY2;
+            elY1 = $(jList[i]).offset().top;
+            var nextOffset = $(jList[i]).next().offset();
+            elY2 = (nextOffset) ? nextOffset.top :
+                (elY1 + jList[i].offsetHeight);
+            var eY = e.pageY;
+            // console.log(elY1, elY2, eY);
+            if(elY1 <= eY && elY2 >= eY) {
+                return i;
+            }
+        }
+        return -1;
+    };
 
     $(document).on('mouseup', function(e) {
         $(document).off('mousemove');
