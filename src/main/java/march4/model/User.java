933493621223525e86247ac6a95e49736f6a0b97
Deplax <Deplax@JeongWonui-MacBook-Pro.local>
2015-04-09 16:44:51 +0900

    kuku로 입성. Spring 사소한 문제 고

---
+++ src/main/java/march4/model/User.java
@@ -0,0 +1,12 @@
+package march4.model;
+
+public class User {
+
+	private int no;
+	private String name;
+	
+	public User(int no, String name) {
+		this.no = no;
+		this.name = name;
+	}
+}
---
+++ src/test/java/net/march4/support/ApplicationContextTest.java
@@ -0,0 +1,42 @@
+package net.march4.support;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.sql.DataSource;
+
+import march4.dao.UserDao;
+import march4.model.User;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/applicationContext.xml")
+public class ApplicationContextTest {
+	private static final Logger log = LoggerFactory.getLogger(ApplicationContextTest.class);
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private UserDao userDao;
+	
+
+	@Test
+	public void dataSource(){
+		assertNotNull(dataSource);
+	}
+	
+	@Test
+	public void findByNo(){
+		User user = userDao.findByNo("2");
+		log.debug("User : {}", user);
+	}
+	
+
+}
