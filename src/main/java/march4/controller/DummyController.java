1bb3691dc03866e4d50cb933053580ec943966f7
Deplax <Deplax@JeongWonui-MacBook-Pro.local>
2015-04-15 15:55:13 +0900

    dummy class 정리 (Json)

--- src/main/java/march4/controller/DummyController.java
+++ src/main/java/march4/controller/DummyController.java
@@ -1,5 +1,10 @@
 package march4.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import march4.model.Dummy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -7,6 +12,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/dummy")
@@ -21,9 +27,25 @@ public class DummyController {
 	}
 	
 	@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public String getMovie(@PathVariable String name, ModelMap model) {
+	public String pages(@PathVariable String name, ModelMap model) {
 		log.debug("Admission to the {} page method!", name);
 		model.addAttribute("dummyName", name);
 		return "dummy";
+	}
+	
+	@RequestMapping(value = "/json", method = RequestMethod.GET)
+	public @ResponseBody Dummy getJson(){
+		Dummy dummy = new Dummy(1, "dummy");
+		log.debug("convert json data : {}", dummy.toString());
+		return dummy;
+	}
+	
+	@RequestMapping(value = "/jsonmap", method = RequestMethod.GET)
+	public @ResponseBody Map<String, Object> getJsonMap(){
+		Map<String, Object> jsonObject = new HashMap<String, Object>();
+		jsonObject.put("no", 1);
+		jsonObject.put("name", "dummy");
+		log.debug("convert json data : {}", jsonObject);
+		return jsonObject;
 	}
 }
