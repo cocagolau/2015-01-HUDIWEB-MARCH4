824bc35590faf96241261dee48f946b7bf401d7f
Chanjin Park <cjmixx@naver.com>
2015-04-22 17:12:46 +0900

    grunt template

--- src/main/frontend/Gruntfile.js
+++ src/main/frontend/Gruntfile.js
@@ -3,24 +3,58 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        dirs: {
+            js : 'js',
+            jsLib : 'js/lib',
+            css : 'css',
+            cssLib : 'css/lib',
+            less : 'less'
+        },
+        fileNames: {
+            bower : '_bower'
+        },
+
         clean: {
             all: [
                 '**/_bower*.*',
-                'js/lib/**/*',
-                'css/**/*'
+                '<%= dirs.jsLib %>/**/*',
+                '<%= dirs.cssLib %>/**/*'
             ]
         },
         
         bower_concat: {
             all: {
-                dest: 'js/lib/_bower.js',
-                cssDest: 'css/lib/_bower.css'
+                dest: '<%= dirs.jsLib %>/<%= fileNames.bower %>.js',
+                cssDest: '<%= dirs.cssLib %>/<%= fileNames.bower %>.css'
             }
         },
+
+        template: {
+            initBuild: {
+                options: {
+                    data: {
+                        jsLibFile : '<%= fileNames.bower %>.js',
+                        jsLibDir : '<%= dirs.jsLib %>',
+                        jsDir : '<%= dirs.js %>',
+                        cssLibDir : '<%= dirs.cssLib %>',
+                        cssDir : '<%= dirs.css %>',
+                        title : 'march4'
+                    }
+                },
+                files: [{
+                    expand: true,
+                    cwd: '.',
+                    src: ['**/*.html.tpl'],
+                    dest: '.',
+                    ext: '.html'
+                }],
+            }
+        }
     });
 
     grunt.loadNpmTasks('grunt-bower-concat');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-template');
     
-    grunt.registerTask('initBuild', ['clean','bower_concat']);
+    grunt.registerTask('initBuild', ['clean','bower_concat','template:initBuild']);
 };
---
+++ src/main/frontend/index.html.tpl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title><%- title %></title>
+    <link rel="stylesheet" href="<%- cssDir %>/style.css">
+    <script src="<%- jsLibDir %>/<%- jsLibFile %>"></script>
+    <script src="<%- jsDir %>/namespaces.js"></script>
+</head>
+<body>
+    <h1>Hello World!</h1>  
+</body>
+</html>
