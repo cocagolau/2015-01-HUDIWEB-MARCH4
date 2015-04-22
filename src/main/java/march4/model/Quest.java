9f7b9d015253d2a320755527c24b900faa11ce04
heesu <heesu@nhnnext.org>
2015-04-22 20:28:57 +0900

    quest 객체 model, dao 구현

---
+++ src/main/java/march4/model/Quest.java
@@ -0,0 +1,88 @@
+package march4.model;
+
+public class Quest {
+	// primitive type(int)은 db의 NULL 값을 만나면 TypeMismatchException 발생.
+	private Integer qId;
+	private Integer pId;
+	private Integer posX;
+	private Integer posY;
+	private Integer order;
+	private String contents;
+	private String due;
+
+	// RowMapper 에서 기본생성자 필요.
+	public Quest(){}
+	
+	public Quest(String contents) {
+		this.contents = contents;
+	}
+
+	public Quest(Integer qId, Integer pId, Integer posX, Integer posY, Integer order,
+			String contents, String due) {
+		this.qId = qId;
+		this.pId = pId;
+		this.posX = posX;
+		this.posY = posY;
+		this.order = order;
+		this.contents = contents;
+		this.due = due;
+	}
+
+	public Integer getqId() {
+		return qId;
+	}
+
+	public void setqId(Integer qId) {
+		this.qId = qId;
+	}
+
+	public Integer getpId() {
+		return pId;
+	}
+
+	public void setpId(Integer pId) {
+		this.pId = pId;
+	}
+
+	public Integer getPosX() {
+		return posX;
+	}
+
+	public void setPosX(Integer posX) {
+		this.posX = posX;
+	}
+
+	public Integer getPosY() {
+		return posY;
+	}
+
+	public void setPosY(Integer posY) {
+		this.posY = posY;
+	}
+
+	public Integer getOrder() {
+		return order;
+	}
+
+	public void setOrder(Integer order) {
+		this.order = order;
+	}
+
+	public String getContents() {
+		return contents;
+	}
+
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+
+	public String getDue() {
+		return due;
+	}
+
+	public void setDue(String due) {
+		this.due = due;
+	}
+	
+	
+}
---
+++ src/test/java/march4/dao/QuestDaoTest.java
@@ -0,0 +1,54 @@
+package march4.dao;
+
+import static org.junit.Assert.assertNotNull;
+
+import javax.sql.DataSource;
+
+import march4.model.Quest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+// "@ComponentScan does not work."
+@ContextConfiguration("classpath:/application-test.xml")
+public class QuestDaoTest {
+	private static final Logger log = LoggerFactory.getLogger(QuestDaoTest.class);
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private QuestDao questDao;
+	
+	@Test
+	public void dataSource(){
+		assertNotNull(dataSource);
+	}
+	
+	@Test
+	public void insert() {
+		Quest quest = new Quest(3, 1, 0, 0, 1, "tt", "2015-01-01 00:00:00");
+		questDao.insert(quest);
+		log.debug("Insert quest : {}", quest);
+	}
+	
+	@Test
+	public void insertContentsOnly() {
+		Quest quest = new Quest("test");
+		questDao.insertContentsOnly(quest);
+		log.debug("Insert quest : {}", quest);
+	}
+	
+	@Test
+	public void select(){
+		Quest quest = questDao.select(18);
+//		List<Quest> quest = questDao.selectAll();
+		log.debug("Select quest : {}", quest);
+	}
+}
