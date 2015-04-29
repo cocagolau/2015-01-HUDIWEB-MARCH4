76cbd15da16a0deed237e9f98798ee4803b5dfe2
Chanjin Park <cjmixx@naver.com>
2015-04-29 09:40:14 +0900

    added some sub pages

--- websrc/js/urlmapping.js
+++ websrc/js/urlmapping.js
@@ -1,20 +1,30 @@
-march4.core = angular.module('march4',['ngRoute']);
-march4.core.config(function($routeProvider, $locationProvider) {
+march4.core = angular.module('march4', ['ngRoute']);
+
+march4.core.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider
         .when('/',{
             templateUrl: '/frontpage.div',
             controller: march4.controller.frontController
         })
-        .when('/world',{
+        .when('/world', {
             templateUrl: '/world.div',
-            controller: march4.controller.worldController
+            controller: 'WorldController'
+        })
+        .when('/world/:worldId', {
+            templateUrl: '/world.div',
+            controller: 'WorldController'
+        })
+        .when('/world/:worldId/project/:projectId', {
+            templateUrl: '/project.div',
+            controller: 'ProjectController'
         })
         .otherwise({
             redirectTo: '/error/404'
         });
 
-        $locationProvider.html5Mode({
-            enabled: true,
-            requireBase: false
-        });
-});
+    $locationProvider.html5Mode(true).hashPrefix('!');
+}]);
+
+
+march4.core.controller('WorldController', march4.controller.worldController);
+march4.core.controller('ProjectController', march4.controller.projectController);
