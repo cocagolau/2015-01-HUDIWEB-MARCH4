489ac32ca2ba55d49141d205e022b7ed5c83d34d
Chanjin Park <cjmixx@naver.com>
2015-05-01 23:18:28 +0900

    added globalController and userController. && some design.

---
+++ websrc/js/main.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    march4.app = angular.module('march4', ['ngRoute'], function($controllerProvider){
+        march4.app.$controllerProvider = $controllerProvider;
+    });
+
+    march4.app.controller('mainController',function($scope){
+    	$scope.user;
+    });
+
+    march4.app.controller('userController',function($scope){
+    	$scope.avatarImg = "http://www.placehold.it/32x32";
+    	$scope.name = "dummyUser";
+    	$scope.email = "dummyUser@gmail.com";
+    	$scope.user = {
+    		avatarImg	: $scope.avatarImg,
+			name 		: $scope.name,
+			email 		: $scope.email
+    	};
+    });
+}());
