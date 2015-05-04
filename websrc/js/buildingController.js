e89e278f25005287b57f09d97637aec45e37032f
deplax <sturdyegg@gmail.com>
2015-05-04 13:18:28 +0900

    나는 커밋을 하지.

--- websrc/js/buildingController.js
+++ websrc/js/buildingController.js
@@ -7,16 +7,16 @@ march4.app.$controllerProvider.register('buildingController', function ($scope, 
         buildingBox: {
             x: 200,
             y: 200,
-            margin: 5
+            margin: 20
         }
     };
-
+    
     $scope.myStyle = function (no) {
 
-        var col = parseInt($(".buildingArea").width() / ($scope.pageSet.buildingBox.x + $scope.pageSet.buildingBox.margin)) + 1;
+        var col = parseInt($(".buildingArea").outerWidth(true) / ($scope.pageSet.buildingBox.x + $scope.pageSet.buildingBox.margin));
 
-        var posX = parseInt(col % (no + 1)) * $scope.pageSet.buildingBox.x + $scope.pageSet.buildingBox.margin;
-        var posY = parseInt(col / (no + 1)) * $scope.pageSet.buildingBox.y + $scope.pageSet.buildingBox.margin;
+        var posX = parseInt(no % col) * $scope.pageSet.buildingBox.x + $scope.pageSet.buildingBox.margin * parseInt(no % col) ;
+        var posY = parseInt(no / col) * $scope.pageSet.buildingBox.y + $scope.pageSet.buildingBox.margin * parseInt(no / col) ;
 
         return {
             left: posX + "px",
@@ -24,7 +24,11 @@ march4.app.$controllerProvider.register('buildingController', function ($scope, 
         }
     };
 
-
+    $scope.recalc = function(no){
+        for(var i = no; i < $scope.Buildings.length; i++){
+            $scope.Buildings[i].no = i;
+        }
+    };
 
 
     //빌딩의 소유자를 보낸다. 
@@ -43,7 +47,7 @@ march4.app.$controllerProvider.register('buildingController', function ($scope, 
                     $scope.Buildings[i].no = i;
                     $timeout(function () {
                         $scope.Buildings[i].hide = false;
-                    }, i * 100);
+                    }, i * 150);
                 })(i);
             }
         }).
@@ -63,9 +67,11 @@ march4.app.$controllerProvider.register('buildingController', function ($scope, 
             data: $scope.addData
         }).
         success(function (data, status, headers, config) {
-            //$window.location.replace('/dummy/ajax');
             $scope.default();
-            //$scope.Dummies = data;
+            //$scope.Buildings.push($scope.data);
+            //$scope.Buildings[$scope.Buildings.length - 1].hide = true;
+            //$scope.Buildings[$scope.Buildings.length - 1].no = $scope.Buildings.length - 1;
+            //$scope.Buildings[$scope.Buildings.length - 1].hide = false;
         }).
         error(function (data, status, headers, config) {
             if (status == 400) {
@@ -85,29 +91,9 @@ march4.app.$controllerProvider.register('buildingController', function ($scope, 
         }).
         success(function (data, status, headers, config) {
             if (data) {
-                $scope.Buildings[no].hide = true;
+                //debugger;
+                $scope.Buildings[no].delete = true;
             }
-        }).
-        error(function (data, status, headers, config) {
-            if (status == 400) {
-                $scope.messages = data;
-            } else {
-                alert('Unexpected server error.');
-            }
-        });
-    };
-
-    $scope.del = function (pid) {
-        $scope.delData.pid = pid;
-        $http({
-            method: 'POST',
-            url: '/building/del',
-            data: $scope.delData
-        }).
-        success(function (data, status, headers, config) {
-            //$window.location.replace('/dummy/ajax');
-            $scope.default();
-            //$scope.Dummies = data;
         }).
         error(function (data, status, headers, config) {
             if (status == 400) {
