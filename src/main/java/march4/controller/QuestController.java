290a641464f403025a8056cbf265d0a0c6177882
heesu <heesu@nhnnext.org>
2015-05-12 19:21:15 +0900

    requestbody with model
    #38

--- src/main/java/march4/controller/QuestController.java
+++ src/main/java/march4/controller/QuestController.java
@@ -1,14 +1,10 @@
 package march4.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import march4.model.Quest;
 import march4.service.QuestService;
 
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.type.TypeReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,34 +41,9 @@ public class QuestController {
 //	}
 	
 	@RequestMapping(value = {""}, method = RequestMethod.POST, headers = {"content-type=application/json"}, produces=MediaType.APPLICATION_JSON_VALUE)
-	public String request(@RequestBody String body, @PathVariable String pId) {
-		log.debug("roadmap POST");
-		System.out.println(body);
-		
-		Map<String,String> map = new HashMap<String,String>();
-		ObjectMapper mapper = new ObjectMapper();
-		Quest newQuest = null;
-		try {
-			map = mapper.readValue(body, new TypeReference<HashMap<String,String>>(){});
-			
-			log.debug(map.toString());
-			
-			newQuest = new Quest(
-					pId,
-					map.get("posX"),
-					map.get("posY"),
-					map.get("order"),
-					map.get("content"),
-					map.get("due")
-			);
-			q.insert(newQuest);
-			
-			log.debug("reach at the end of try");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println("reach at the end of method");
+	public String request(@RequestBody Quest quest, @PathVariable int pId) {
+		quest.setpId(pId);
+		q.insert(quest);
 		return "{\"result\":true}";
 	}
 }
